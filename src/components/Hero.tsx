e158066e224dac2de3e34c6ca712e8cdb0d79b76
Quratulain-Yasir <hiraiqbal681@gmail.com>
2026-03-11 00:46:05 +0500

    ting part of ui fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <span className="font-one tracking-widest text-sm lg:text-base ">Available for freelance · 2026</span>
       </div> */}
       
-        <h1 className="text-5xl lg:text-6xl leading-tighter mt-10 lg:mt-12 lg:mb-6 font-bold">
+        <h1 className="text-4xl lg:text-6xl leading-tighter mt-10 lg:mt-12 lg:mb-6 font-bold">
           <span className="font-two">Your business, </span>
           <br />
           <span className="italic font-two pl-10">finally</span>
@@ -24,13 +24,13 @@ const Hero = () => {
         </p>
 <div className="flex items-center gap-6 justify-between px-2 pb-10 lg:pb-5 lg:py-5">
   
-        <button className="h-[8vh] sm:h-[9vh] border-2 border-[rgba(40, 19, 90, 1)] pl-5 pr-2 py-1 rounded-full text-[#d8cfc4] hover:text-[#d8cfc4] shadow-md transition font-one text-sm lg:text-lg inline-flex gap-2 sm:gap-3 items-center hover:scale-105 focus:scale-105 duration-300 ease-in-out mt-4 z-10">
+        <button className="h-[8vh] sm:h-[9vh] border-2 border-[rgba(40, 19, 90, 1)] pl-5 pr-2 py-1 rounded-full text-[#d8cfc4] hover:text-[#d8cfc4] shadow-md transition font-one text-xs lg:text-lg inline-flex gap-2 sm:gap-3 items-center hover:scale-105 focus:scale-105 duration-300 ease-in-out mt-4 z-10">
           Let’s Build Your Website
           <span className="bg-[#d8cfc4] rounded-full px-1 py-1 text-black">
             <ArrowRight className="w-4 h-4" />
           </span>
         </button>
-       <div className="stack-note lg:text-base text-sm text-[#d8cfc4] lg:px-8">
+       <div className="stack-note lg:text-base text-xs text-[#d8cfc4] lg:px-8">
         React & Node.js<br/>
         Vercel · REST APIs
       </div>
@@ -42,7 +42,7 @@ const Hero = () => {
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
-        className="w-[75vw] lg:w-[40vw] h-full transform hover:rotate-1 hover:scale-105 transition focus:scale-102 duration-300 ease-in-out relative"
+        className="w-[82vw] lg:w-[40vw] h-full transform hover:rotate-1 hover:scale-105 transition focus:scale-102 duration-300 ease-in-out relative"
       >
         <div className="absolute lg:w-100 lg:h-100 w-55 h-65 bg-purple-800 blur-3xl rounded-full"></div>
         <img
@@ -51,7 +51,7 @@ const Hero = () => {
           className="h-full w-full object-cover rounded-md shadow-lg relative  rounded-3xl shadow-2xl"
         />
       </motion.div>
-       <div className="absolute lg:bottom-0 bottom-6 right-[0.5px] bg-purple-800 text-white text-[0.68rem] tracking-[0.2em] uppercase lg:px-5 lg:py-3 px-3 py-2 lg:font-medium font-light lg:mr-12 mr-4 rounded-full">Web Developer · Faisalabad</div>
+       <div className="absolute lg:bottom-0 bottom-29 right-[0.5px] bg-purple-800 text-white text-[0.68rem] tracking-[0.2em] uppercase lg:px-5 lg:py-3 px-3 py-2 lg:font-medium font-light lg:mr-12 mr-2 rounded-full">Web Developer · Faisalabad</div>
     </section>
   );
 };
